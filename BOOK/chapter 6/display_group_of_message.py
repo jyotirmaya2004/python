fd9e4f25ea91245dbcc4b7137938fb49a87e8991
jyotirmaya2004 <jyotirmaybehera2020@mail.com>
2024-12-10 18:02:00 +0530

    a

--- BOOK/chapter 6/display_group_of_message.py
+++ BOOK/chapter 6/display_group_of_message.py
@@ -4,5 +4,5 @@ if str=='yes'or str=='Yes':
     print("\tBeing single can give you more freedom ")
     print("\tAnd the ability to focous on your own needs and intrests")
 if str=='no'or str=='No':
-    print("\tPeople in relationships can be happier and healthier than student")
+    print("\tPeople in relationships can be happier and healthier than single")
     print("\tThey can form close bonds and shared values")
