f1a07c4d1b2b0f84d7c14294370856361a4917ab
jyotirmaya2004 <jyotirmaybehera2020@mail.com>
2025-02-14 10:47:30 +0530

    14/2/2025

---
+++ BOOK/chapter 17 FILES IN PYTHON/EMP.py
@@ -0,0 +1,10 @@
+class EMP:
+    def __init__(self,id,name,sal):
+        self.id=id
+        self.name=name
+        self.sal=sal
+    def display(self):
+        print("ID : ",self.id)
+        print("Name : ",self.name)
+        print("Salary : ",self.sal)
+        
